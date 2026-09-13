3f9da3c923d4030397740e305df11f9b7c2b3061
jaweed3 <wedjaw22@gmail.com>
2026-09-13 21:50:40 +0700

    feat: add auditcustomer type

---
+++ audit-service/consumer/consumer.go
@@ -0,0 +1,19 @@
+// Package consumer is for make event and consume nats subject :)
+package consumer
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"time"
+
+	"github.com/jaweed3/mini-microservice/audit-service/repository"
+	"github.com/jaweed3/mini-microservice/contract"
+	"github.com/nats-io/nats.go"
+	"github.com/nats-io/nats.go/jetstream"
+)
+
+type AuditConsumer struct {
+	nc      *nats.Conn
+	service *repository.AuditService
+}
